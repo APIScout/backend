4ef8c20b6cffb1f1dd248b15dbb3b2ad77145ba7
edoriggio <edo.riggio19@gmail.com>
2024-08-22 13:39:17 +0200

    fixed some issues

--- app/internal/controller/dsl.go
+++ app/internal/controller/dsl.go
@@ -13,7 +13,7 @@ import (
 //	@Tags			DSL
 //	@Produce		json
 //	@Param			page		parameter		string						false	"parameter name"
-//	@Success		200			{object}	models.TypesMap|models.TypeToOperatorMap
+//	@Success		200			{object}	models.TypesMap|[]string
 //	@Failure		400			{object}	models.HTTPError
 //	@Failure		500			{object}	models.HTTPError
 //	@Router			/search [post]
